5f1639fb35b25d1ea9240e7520b0a1bd16c6a087
moriisaac <wesongamori@gmail>
2023-11-10 12:34:36 +0300

    Update Settings

--- empins2023/settings.py
+++ empins2023/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY",get_random_secret_key())
 API_KEY_CUSTOM_HEADER = "HTTP_X_API_KEY"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-DEVELOPMENT_MODE = True
+DEVELOPMENT_MODE = False
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
