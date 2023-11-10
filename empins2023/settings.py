0cd252929944372892550709a255b7ab63e90c53
moriisaac <wesongamori@gmail>
2023-11-10 12:23:47 +0300

    Update Settings

--- empins2023/settings.py
+++ empins2023/settings.py
@@ -74,8 +74,8 @@ SIMPLE_API_KEY = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
