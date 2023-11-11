db7ae0d4bc137933c5b922ec2ec5b89b7ce1a4f0
moriisaac <wesongamori@gmail>
2023-11-11 11:09:14 +0300

    New Feat: Swagger Documentation

--- empins2023/urls.py
+++ empins2023/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="AWS-S3 Amazon Storage Bucket API",
+        default_version='v1',
+        description="Store Your Files Securely using AWS-S3-Django API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     # path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('storage/', include('s3storage.urls')),
--- s3storage/urls.py
+++ s3storage/urls.py
@@ -3,23 +3,10 @@
 from django.urls import path, include
 from .views import S3ListBucket, S3CreateBucket, S3UploadFile, S3DownloadFile, S3DeleteFile, S3DeleteBucket
 # from rest_framework_swagger.views import get_swagger_view
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="AWS-S3 Amazon Storage Bucket API",
-        default_version='v1',
-        description="Store Your Files Securely using AWS-S3-Django API",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
-    path('api/swagger', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),
+
 
     # path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('api/list-buckets/', S3ListBucket.as_view(), name='list-buckets'),
