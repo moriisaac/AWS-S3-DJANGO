492713bbe452e5ce6092740cdc44a6532e5a2959
moriisaac <wesongamori@gmail>
2023-11-11 11:29:19 +0300

    New Feat: Swagger Documentation

--- empins2023/urls.py
+++ empins2023/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/swagger', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
 
